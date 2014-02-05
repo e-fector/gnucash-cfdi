1ccd0f71d707be453865cc2f92204dbda5192777
Sebastian Aviña <sebastianavina@gmail.com>
2014-02-04 23:17:42 -0600

    Ya obtiene los datos de la factura

--- timbrador.py
+++ timbrador.py
@@ -11,4 +11,52 @@ import re
 
 from mail import mail
 
+
+from xml.dom.minidom import parse
+
+import sys
+import getopt
+
+import gnucash
+from gnucash.gnucash_business import Customer, Employee, Vendor, Job, \
+    Address, Invoice, Entry, TaxTable, TaxTableEntry, GNC_AMT_TYPE_PERCENT, \
+    GNC_DISC_PRETAX
+
+input_url = "/home/sebastianavina/prueba.gnucash"
+
+try:
+    session = gnucash.Session(input_url,ignore_lock=True)
+except Exception as exception:
+    print "Problem opening input."
+    print exception
+
+factura = session.book.InvoiceLookupByID("124")
+
+print factura.GetOwner().GetName()
+print factura.GetOwner().GetAddr().GetAddr1()   
+print factura.GetOwner().GetAddr().GetAddr2()   
+print factura.GetOwner().GetAddr().GetAddr3()   
+print factura.GetOwner().GetAddr().GetAddr4()   
+
+#print dir(factura)
+
+for concepto in factura.GetEntries():
+    www = Entry(instance=concepto)
+    print www.GetAction()
+    print www.GetDescription()
+    print gnucash.GncNumeric(instance=www.GetQuantity())
+    print gnucash.GncNumeric(instance=www.GetInvPrice())
+    print www.GetInvTaxIncluded()
+    print TaxTable(instance=www.GetInvTaxTable())
+    print www.GetInvTaxable()
+    print www.GetNotes()
+    print www.GetOrder()
+    print dir(www)
+    print gnucash.GncNumeric(instance=www.ReturnTaxValue(www.GetInvTaxable()))
+    print gnucash.GncNumeric(instance=www.ReturnValue(www.GetInvTaxable()))
+
+    
+    
+    
+
 print "Y así empieza"
