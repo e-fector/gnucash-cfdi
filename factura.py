32f4df5aab3bca011215e8fef57b54143e84c543
Sebastian Aviña <sebastianavina@gmail.com>
2014-02-06 21:54:55 -0600

    Se corrigio el error de la manera en invocar el dict

--- factura.py
+++ factura.py
@@ -60,7 +60,7 @@ class Factura():
         self.lineas_factura = []
 
     def agrega_linea(self,cantidad,unidad,noIdentificacion,descripcion,valorUnitario, \
-                         importe,importe_impuesto,tasa):
+                         importe,impuesto,importe_impuesto,tasa):
             self.lineas_factura.append( {
                     "cantidad":cantidad,
                     "unidad":unidad,
@@ -80,11 +80,11 @@ class Factura():
     #
     ###
     def edita_factura_fecha_factura(self, fecha_factura):
-        self.factura.fecha_factura = fecha_factura
+        self.factura["fecha_factura"] = fecha_factura
     def edita_factura_serie(self, serie):
-        self.factura.serie = serie
+        self.factura["serie"] = serie
     def edita_factura_folio(self, folio):
-        self.factura.folio = folio
+        self.factura["folio"] = folio
 
     
 
@@ -94,15 +94,15 @@ class Factura():
     #
     ###
     def edita_encabezado_forma_de_pago(self, forma_de_pago):
-        self.encabezado.forma_de_pago = forma_de_pago
+        self.encabezado["forma_de_pago"] = forma_de_pago
     def edita_encabezado_metodo_de_pago(self, metodo_de_pago):
-        self.encabezado.metodo_de_pago = metodo_de_pago
+        self.encabezado["metodo_de_pago"] = metodo_de_pago
     def edita_encabezado_condiciones(self, condiciones):
-        self.encabezado.condiciones = condiciones
+        self.encabezado["condiciones"] = condiciones
     def edita_encabezado_NumCtaPago(self, NumCtaPago):
-        self.encabezado.NumCtaPago = NumCtaPago
+        self.encabezado["NumCtaPago"] = NumCtaPago
     def edita_encabezado_lugarDeExpedicion(self, lugarDeExpedicion):
-        self.encabezado.lugarDeExpedicion = lugarDeExpedicion
+        self.encabezado["lugarDeExpedicion"] = lugarDeExpedicion
 
     ###
     #
@@ -110,35 +110,41 @@ class Factura():
     #
     ###
     def edita_rfc_receptor(self, rfc_receptor):
-        self.receptor.rfc_receptor = rfc_receptor
+        self.receptor["rfc_receptor"] = rfc_receptor
     def edita_receptor_nombre_fiscal(self, nombre_fiscal):
-        self.receptor.nombre_fiscal = nombre_fiscal
+        self.receptor["nombre_fiscal"] = nombre_fiscal
     def edita_receptor_calle(self, calle):
-        self.receptor.calle = calle
+        self.receptor["calle"] = calle
     def edita_receptor_noExterior(self, noExterior):
-        self.receptor.noExterior = noExterior
+        self.receptor["noExterior"] = noExterior
     def edita_receptor_noInterior(self, noInterior):
-        self.receptor.noInterior = noInterior
+        self.receptor["noInterior"] = noInterior
     def edita_receptor_colonia(self, colonia):
-        self.receptor.colonia = colonia
+        self.receptor["colonia"] = colonia
     def edita_receptor_municipio(self, municipio):
-        self.receptor.municipio = municipio
+        self.receptor["municipio"] = municipio
     def edita_receptor_estado(self, estado):
-        self.receptor.estado = estado
+        self.receptor["estado"] = estado
     def edita_receptor_pais(self, pais):
-        self.receptor.pais = pais
+        self.receptor["pais"] = pais
     def edita_receptor_cp(self, cp):
-        self.receptor.cp = cp
+        self.receptor["cp"] = cp
     def edita_receptor_noCliente(self, noCliente):
-        self.receptor.noCliente = noCliente
+        self.receptor["noCliente"] = noCliente
     def edita_receptor_email(self, email):
-        self.receptor.email = email
+        self.receptor["email"] = email
 
 
     def __str__(self):
-        return genera_layout(self.encabezado, self.receptor, self.factura, \
-                                 self.lineas_factura, self.emisor)
+        return genera_layout(encabezado = self.encabezado,
+                             receptor = self.receptor, 
+                             factura = self.factura, 
+                             lineas_factura = self.lineas_factura, 
+                             emisor = self.emisor)
 
     def __unicode__(self):
-        return genera_layout(self.encabezado, self.receptor, self.factura, \
-                                 self.lineas_factura, self.emisor)
+        return genera_layout(encabezado = self.encabezado,
+                             receptor = self.receptor, 
+                             factura = self.factura, 
+                             lineas_factura = self.lineas_factura, 
+                             emisor = self.emisor)
